e968af86d215b3c75453c2d1f5454cba4833317b
Max <maxh2709@gmail.com>
2021-03-15 00:40:48 +0100

    Make event variables public where possible

--- src/main/java/com/feed_the_beast/mods/ftbschools/kubejs/SchoolEventJS.java
+++ src/main/java/com/feed_the_beast/mods/ftbschools/kubejs/SchoolEventJS.java
@@ -4,19 +4,17 @@ import com.feed_the_beast.mods.ftbschools.data.SchoolData;
 import com.feed_the_beast.mods.ftbschools.data.SchoolType;
 import dev.latvian.kubejs.entity.EntityJS;
 import dev.latvian.kubejs.player.PlayerEventJS;
-import dev.latvian.kubejs.player.ServerPlayerJS;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
 
 public abstract class SchoolEventJS extends PlayerEventJS {
 
-    private final SchoolData school;
-    private final ServerPlayer player;
+    public final SchoolData school;
+    private final ServerPlayer _player;
 
     protected SchoolEventJS(SchoolData school, ServerPlayer player) {
         this.school = school;
-        this.player = player;
+        this._player = player;
     }
 
     public final SchoolData getData() {
@@ -37,7 +35,7 @@ public abstract class SchoolEventJS extends PlayerEventJS {
 
     @Override
     public EntityJS getEntity() {
-        return this.entityOf(player);
+        return this.entityOf(_player);
     }
 
     public static class Enter extends SchoolEventJS {
@@ -47,7 +45,7 @@ public abstract class SchoolEventJS extends PlayerEventJS {
     }
 
     public static class Leave extends SchoolEventJS {
-        private final boolean droppedOut;
+        public final boolean droppedOut;
 
         public Leave(SchoolData school, ServerPlayer player, boolean droppedOut) {
             super(school, player);
